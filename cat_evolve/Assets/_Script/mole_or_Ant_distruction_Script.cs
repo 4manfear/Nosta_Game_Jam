c7c0c3915871da6aaf6d7000fedc510d78d88eaa
4manfear <rickroad774@gmail.com>
2024-11-10 06:00:56 +0530

    1

---
+++ cat_evolve/Assets/_Script/mole_or_Ant_distruction_Script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mole_or_Ant_distruction_Script : MonoBehaviour
+{
+    public Money_manager manager;
+
+    private void Update()
+    {
+        manager = FindObjectOfType<Money_manager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Guard"))
+        {
+            Debug.Log("collision happend");
+
+            manager.current_Money_holds += 20;
+
+            Destroy(this.gameObject);
+        }
+    }
+}
